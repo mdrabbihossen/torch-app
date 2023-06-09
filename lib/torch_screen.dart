c60a75b3b0290ed1b7aadc0ac26de432358004dc
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-09 16:21:25 +0600

    first commit

---
+++ lib/torch_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TorchScreen extends StatefulWidget {
+  const TorchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TorchScreen> createState() => _TorchScreenState();
+}
+
+class _TorchScreenState extends State<TorchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
