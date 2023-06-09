4e2c8233658f62f7ba481a2b59de7a3070b1dcbf
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-09 17:52:35 +0600

    assets

--- lib/torch_screen.dart
+++ lib/torch_screen.dart
@@ -10,6 +10,10 @@ class TorchScreen extends StatefulWidget {
 class _TorchScreenState extends State<TorchScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlashLight'),
+      ),
+    );
   }
 }
